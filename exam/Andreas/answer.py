1e72029363fbbbdaa5c8de08965c038189ea30cf
forzadata <aniel21@student.sdu.dk>
2023-01-24 15:45:14 +0100

    Andreas files

---
+++ exam/Andreas/answer.py
@@ -0,0 +1,4 @@
+import os
+os.chdir("c:/Users/andly/Desktop/DATA SCIENCE NOTER/AML/exam/applied_ML_faelles/exam/Andreas/")
+print("Path..." + os.getcwd())
+#os.system("andreas_env\\Scripts\\activate.bat")
